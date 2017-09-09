3850062b309f60f6f9ed60c3a464c33e944ae9ec
roxmulder <we.are.torchwood@gmail.com>
2017-09-09 13:32:55 -0300

    primer commit

---
+++ functions.h
@@ -0,0 +1,13 @@
+/*
+ * functions.h
+ *
+ *  Created on: 9/9/2017
+ *      Author: utnso
+ */
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+int sumar(int n1, int n2);
+
+#endif /* FUNCTIONS_H_ */
